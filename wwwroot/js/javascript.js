da874f9afc5642a0c92772f79ec7c3828c4838cc
EpicWerf <62124316+EpicWerf@users.noreply.github.com>
2021-01-26 13:10:43 -0700

    Bug Fixes

--- wwwroot/js/javascript.js
+++ wwwroot/js/javascript.js
@@ -8,14 +8,53 @@
 
     //set up some blank variables to be used later on
     var iFinalGrade = 0;
+    var sFinalLetter = '';
     var sOutput = '';
 
     //calculate the final grade based on grade weights and what the user inputted
     iFinalGrade = ((iAssignmentsGrade * .5) + (iGroupProjectsGrade * .1) + (iQuizzesGrade * .1) + (iExamsGrade * .2) + (iIntexGrade * .1));
 
+    //convert the final grade into a letter
+    if (iFinalGrade >= 94) {
+        sFinalLetter = 'A'
+    }
+    else if (iFinalGrade >= 94) {
+        sFinalLetter = 'A-'
+    }
+    else if (iFinalGrade >= 94) {
+        sFinalLetter = 'B+'
+    }
+    else if (iFinalGrade >= 94) {
+        sFinalLetter = 'B'
+    }
+    else if (iFinalGrade >= 94) {
+        sFinalLetter = 'B-'
+    }
+    else if (iFinalGrade >= 94) {
+        sFinalLetter = 'C+'
+    }
+    else if (iFinalGrade >= 94) {
+        sFinalLetter = 'C'
+    }
+    else if (iFinalGrade >= 94) {
+        sFinalLetter = 'C-'
+    }
+    else if (iFinalGrade >= 94) {
+        sFinalLetter = 'D+'
+    }
+    else if (iFinalGrade >= 94) {
+        sFinalLetter = 'D'
+    }
+    else if (iFinalGrade >= 94) {
+        sFinalLetter = 'D-'
+    }
+    else {
+        sFinalLetter = 'E'
+    }
+
     //create an output variable
-    sOutput = ('Based on the grades that you inputted, you will end up with a ' + iFinalGrade + ' in the class');
+    sOutput = ('Based on the grades that you inputted, you will end up with a ' + iFinalGrade + ' which is a(n) ' + sFinalLetter + ' in the class');
 
     //alert with final grade
-    alert(sOutput);
+    alert(sOutput)
 });
